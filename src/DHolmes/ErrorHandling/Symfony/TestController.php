d20d23f4fc28b677fe95f43449e7436b3fec8f1f
Daniel Holmes <daniel@creatio.com.au>
2012-06-05 00:00:23 +1000

    Error handling index page for symfony

--- src/DHolmes/ErrorHandling/Symfony/TestController.php
+++ src/DHolmes/ErrorHandling/Symfony/TestController.php
@@ -3,9 +3,39 @@
 namespace DHolmes\ErrorHandling\Symfony;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class TestController
 {
+    /** @var UrlGeneratorInterface */
+    private $urlGenerator;
+    
+    /** @param UrlGeneratorInterface $urlGenerator */
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+    
+    /** @return Response */
+    public function indexAction()
+    {
+        $types = array('notice', 'warning', 'exception', 'fatal', 'error');
+        $content = '<h3>Error Testing</h3>';
+        $content .= '<ul>';
+        foreach ($types as $type)
+        {
+            $url = $this->urlGenerator->generate(sprintf('errorTest%s', ucfirst($type)));
+            $content .= '<li>' . strtoupper($type) . ': ';
+            $content .= sprintf('<a href="%s">GET</a>', $url) . ' | ';
+            $content .= sprintf('<form action="%s" method="post" style="display:inline">
+                                    <input type="submit" value="POST" />
+                                </form>', $url);
+            $content .= '</li>';
+        }
+        $content .= '</ul>';
+        return new Response($content);
+    }
+    
     /** @return Response */
     public function exceptionAction()
     {

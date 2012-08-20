31d0ea4df4655e37e219af3279cc5bbeca46d815
Daniel Holmes <daniel@creatio.com.au>
2012-08-20 01:18:56 -0700

    Filter out error loading script js errors

--- src/DHolmes/ErrorHandling/Resources/public/js/handling.js
+++ src/DHolmes/ErrorHandling/Resources/public/js/handling.js
@@ -2,25 +2,39 @@ var DHolmes = DHolmes||{};
 DHolmes.ErrorHandling = DHolmes.ErrorHandling||{};
 DHolmes.ErrorHandling.createNotifyUrlErrorHandler = function(endPointUrl) {
 	return function(message, url, lineNumber) {
-		var request = null;
-		if (window.ActiveXObject) { // IE
-			request = new ActiveXObject("Microsoft.XMLHTTP");
-		} else {
-			request = new XMLHttpRequest();
-		}
-		request.open('POST', endPointUrl, true);
-		request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		var params = {
+		//transform errors
+        if (typeof(message) === 'object' && message.srcElement && message.target) {
+            if (message.srcElement == '[object HTMLScriptElement]' && message.target == '[object HTMLScriptElement]') {
+                message = 'Error loading script';
+            } else {
+                message = 'Event Error - target:' + message.target + ' srcElement:' + message.srcElement;
+            }
+        }
+
+        message = message.toString();
+
+        // ignore errors
+        /*if (message.indexOf("Location.toString") > -1) {
+            return;
+        }*/
+        if (message.indexOf("Error loading script") > -1) {
+            return;
+        }
+
+        //report errors
+        window.onerror = function(){};
+        var params = {
 			"message": message,
 			"scriptUrl": url,
 			"lineNumber": lineNumber,
 			"cookie": document.cookie,
-			"url": document.URL
+			"url": document.URL,
+			"noCache": (new Date())
 		};
 		var queryComps = new Array();
 		for (var key in params) {
 			queryComps.push(encodeURIComponent(key) + "=" + encodeURIComponent(params[key]));
 		}
-		request.send(queryComps.join("&"));
+		(new Image()).src = endPointUrl + "?" + queryComps.join("&");
 	};
 };

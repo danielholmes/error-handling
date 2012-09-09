094be5a6e942e2a083452376b006be909748d67e
= <daniel@creatio.com.au>
2012-09-09 12:07:36 +1000

    Added getter for responders

--- src/DHolmes/ErrorHandling/ErrorHandler.php
+++ src/DHolmes/ErrorHandling/ErrorHandler.php
@@ -8,24 +8,30 @@ class ErrorHandler
     private static $NON_ERROR_HANDLER_TYPES = array(
         E_ERROR, E_PARSE, E_CORE_ERROR, E_CORE_WARNING, E_COMPILE_ERROR, E_COMPILE_WARNING
     );
-    
+
     /** @var array */
     private $responders;
     /** @var boolean */
     private $registered;
-    
+
     public function __construct()
     {
         $this->registered = false;
         $this->setResponders(array(new SimpleDisplayErrorResponder()));
     }
-    
+
     /** @param array $responders */
     public function setResponders(array $responders)
     {
         $this->responders = $responders;
     }
-    
+
+    /** @return array */
+    public function getResponders()
+    {
+        return $this->responders;
+    }
+
     /** @return array */
     private function getNonDisplayResponders()
     {
@@ -34,45 +40,46 @@ class ErrorHandler
             return !($responder instanceof DisplayErrorResponder);
         }));
     }
-    
+
     /** @param array $responders */
     public function appendResponders(array $responders)
     {
         $this->responders = array_merge($this->responders, $responders);
     }
-    
+
     /** @param ExceptionResponder $responder */
     public function appendResponder(ExceptionResponder $responder)
     {
         $this->responders[] = $responder;
     }
-    
+
+    /** @return ExceptionResponder */
     public function popResponder()
     {
         return array_pop($this->responders);
     }
-    
+
     public function clearResponders()
     {
         $this->responders = array();
     }
-    
+
     public function register()
     {
         $this->registered = true;
-    
+
         set_error_handler(array($this, 'handleError'));
         register_shutdown_function(array($this, 'handleShutdown'));
         set_exception_handler(array($this, 'handleException'));
     }
-    
+
     /**
      * @param int $level
      * @param string $message
      * @param string $file
      * @param int $line
      * @param array $context
-     * @return boolean 
+     * @return boolean
      */
     public function handleError($level, $message, $file, $line, $context)
     {
@@ -85,13 +92,13 @@ class ErrorHandler
 
         return false;
     }
-    
+
     /** @param \Exception $exception */
     public function handleException(\Exception $exception)
     {
         $this->respondToException($exception);
     }
-    
+
     /** @param \Exception $exception */
     public function handleExceptionWithoutDisplayResponders(\Exception $exception)
     {
@@ -100,7 +107,7 @@ class ErrorHandler
             $responder->respond($exception);
         }
     }
-    
+
     /**
      * @param int $type
      * @return boolean
@@ -109,7 +116,7 @@ class ErrorHandler
     {
         return in_array($type, self::$NON_ERROR_HANDLER_TYPES, true);
     }
-    
+
     public function handleShutdown()
     {
         $lastError = error_get_last();
@@ -122,14 +129,14 @@ class ErrorHandler
             $this->respondToSystemException($e);
         }
     }
-    
+
     /** @param \Exception $e */
     protected function respondToSystemException(\Exception $e)
     {
         $this->unregister();
         $this->respondToException($e);
     }
-    
+
     /** @param \Exception $e */
     private function respondToException(\Exception $e)
     {
@@ -138,33 +145,32 @@ class ErrorHandler
             $responder->respond($e);
         }
     }
-    
+
     /**
-     * Unregister all handlers so don't get stuck in loops and the like 
+     * Unregister all handlers so don't get stuck in loops and the like
      */
     public function unregister()
     {
         register_shutdown_function(array($this, 'nullHandler'));
         set_error_handler(array($this, 'nullHandler'));
         set_exception_handler(array($this, 'nullHandler'));
-        
+
         $this->registered = false;
     }
-    
+
     /** @return boolean */
     public function isRegistered()
     {
         return $this->registered;
     }
-    
+
     /**
      * Used for redirecting error handlers to have no result
      */
     public function nullHandler()
     {
-        
     }
-    
+
     /**
      * @param array $responders
      * @return ErrorHandler
@@ -174,7 +180,7 @@ class ErrorHandler
         $handler = new static();
         $handler->appendResponders($responders);
         $handler->register();
-        
+
         return $handler;
     }
 }
